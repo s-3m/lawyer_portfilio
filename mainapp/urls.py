d4d1bb5128df2bb7ce4d7e46cd003bd552b11cd8
roman <s-3m@yandex.ru>
2025-05-27 16:52:20 +0300

    add static files

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
+from mainapp import views
 
-
+app_name = "mainapp"
 urlpatterns = [
-    path("", include("mainapp.urls")),
-    path("admin/", admin.site.urls),
+    path(
+        "",
+        views.IndexView.as_view(),
+        name="index",
+    ),
 ]
 
 if settings.DEBUG:
