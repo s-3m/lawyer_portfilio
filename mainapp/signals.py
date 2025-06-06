7cf3572f59564e0d6eda79a040965853d279c3c8
roman <s-3m@yandex.ru>
2025-06-06 14:22:21 +0300

    add Review signal and fix icons path in template

--- mainapp/signals.py
+++ mainapp/signals.py
@@ -1,15 +1,30 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from mainapp.models import Request
-from .tasks import send_request_email
+from mainapp.models import Request, Review
+from .tasks import send_email_notify
 
 @receiver(post_save, sender=Request)
 def notify_new_request(sender, instance, created, **kwargs):
     if created:
         subject = f"Запрос на {instance.get_app_type_display()}"
-        message = f"Тип: {instance.get_app_type_display()}\nИмя: {instance.name}\n"\
-                  f"Телефон: {instance.phone}\n"\
-                  f"Направление: {instance.get_case_display()}\n"\
-                  f"Сообщение: {instance.message}\n"
+        message = (f"Тип: {instance.get_app_type_display()}\n"
+                   f"Имя: {instance.name}\n"
+                   f"Регион: {instance.get_region_display()}\n"
+                   f"Телефон: {instance.phone}\n"
+                   f"Направление: {instance.get_case_display()}\n"
+                   f"Сообщение: {instance.message}\n")
 
-        send_request_email.delay(subject, message, "admin@site.com", ["admin@site.com"])
+        send_email_notify.delay(subject, message, "admin@site.com", ["admin@site.com"])
+
+
+@receiver(post_save, sender=Review)
+def notify_new_review(sender, instance: Review, created, **kwargs):
+    if created:
+        subject = f"Новый отзыв"
+        message = ("Новый отзыв ожидает модерации. Отзыв не появится на сайте, пока не пройдёт вашу модерацию!\n"
+                   f"Отзыв:\n{instance.text}\n"
+                   f"От: {instance.name}\n"
+                   f"Направление: {instance.get_case_display()}\n")
+
+
+        send_email_notify.delay(subject, message, "admin@site.com", ["admin@site.com"])
--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 
 @shared_task
-def send_request_email(subject, message, from_email, recipient_list):
+def send_email_notify(subject, message, from_email, recipient_list):
     send_mail(
         subject,
         message,
